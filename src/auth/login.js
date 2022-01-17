bcb844eaa37d6ae9051e97d4a4744c34dd11db57
devnote-dev <pokeraven4@gmail.com>
2022-01-17 20:02:15 +0000

    feat(auth): implement panel data fetch

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,4 +1,5 @@
 const db = require('../functions/db');
+const panel = require('../functions/panel');
 
 module.exports = async (request, reply) => {
     const data = request.body;
@@ -7,14 +8,23 @@ module.exports = async (request, reply) => {
         const account = await db.fetchAccount(data.email);
         if (!account) return reply.redirect('/login?err=NOACCOUNT');
         if (data.password !== account.password) return reply.redirect('/login?err=INVALIDPASS');
-        request.session.set('account', account);
+
+        const panelData = (await panel.fetchAccount(data.email)).data[0].attributes;
+        request.session.set('account', Object.assign(account, {
+            root_admin: panelData.root_admin,
+            servers: panelData.relationships.servers.data,
+            is_new: false
+        }));
+
         return reply.redirect('/dashboard');
 
     } else if (request.url === '/auth/signup') {
         let account = await db.fetchAccount(data.email);
         if (account) return reply.redirect('/login?err=ACCEXISTS');
+
         account = await db.createAccount(data);
         if (!account) return reply.view('err500.ejs');
+
         request.session.set('account', account);
         return reply.redirect('/dashboard');
 
