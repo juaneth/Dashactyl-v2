c0e46152d2417aec1a4630e3b1c6d5d5090e53e7
devnote-dev <pokeraven4@gmail.com>
2022-01-22 11:53:56 +0000

    feat(api): implement api auth, fix typo-typo

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,35 @@
+const { loadSettings } = require('../functions');
+
+const settings = loadSettings();
+
 module.exports = (api, _, done) => {
+    api.addHook('preHandler', (request, reply, done) => {
+        const auth = request.headers['authorization']?.slice(7);
+
+        if (!auth) {
+            reply.send({
+                status: 'error',
+                message: 'missing authorization'
+            });
+            return done();
+        }
+
+        if (!settings.api.keys.includes(auth)) {
+            reply.send({
+                status: 'error',
+                message: 'invalid authorization'
+            });
+            return done();
+        }
+
+        done();
+    });
+
     api.register(
         (ctx, _, done) => require('./users')(ctx, done),
         { prefix: '/users' }
     );
+
     api.register(
         (ctx, _, done) => require('./coupons')(ctx, done),
         { prefix: '/coupons' }
