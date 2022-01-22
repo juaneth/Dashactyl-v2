5998a81f0d01123e158555141c2af5b299ba65b4
devnote-dev <pokeraven4@gmail.com>
2022-01-22 14:26:08 +0000

    feat(api): implement redeem endpoint & response fixes

--- src/api/coupons.js
+++ src/api/coupons.js
@@ -3,6 +3,7 @@ const db = require('../functions/db');
 module.exports = async (api, done) => {
     api.get('/', async (_, reply) => {
         const data = await db.getAllCoupons();
+        for (const c of data) delete c._id;
         return reply.send({ status: 'success', data });
     });
 
@@ -18,12 +19,13 @@ module.exports = async (api, done) => {
             message: 'coupon not found'
         });
 
+        delete data._id;
         return reply.send({ status: 'success', data });
     });
 
     api.post('/', async (request, reply) => {
         if (
-            !['coins', 'memory', 'disk', 'cpu', 'servers']
+            !['coins', 'ram', 'disk', 'cpu', 'servers']
             .some(k => k in request.body)
         ) return reply.send({
             status: 'error',
@@ -40,7 +42,9 @@ module.exports = async (api, done) => {
         data.servers = validate(data.servers, 10);
 
         data.uses = validate(data.uses, 10_000);
+        data.uses ||= null;
         data.expires_at = validate(data.expires_at, 2.628E+09);
+        data.expires_at ||= null;
 
         await db.createCoupon({
             ...data,
@@ -49,6 +53,56 @@ module.exports = async (api, done) => {
         const res = await db.fetchCoupon(data.code);
         delete res._id;
         return reply.send({ status: 'success', data: res });
+    });
+
+    api.post('/:code/redeem', async (request, reply) => {
+        if (!request.params.code?.length) return reply.send({
+            status: 'error',
+            message: 'missing parameter code'
+        });
+
+        if (!request.body.user) return reply.send({
+            status: 'error',
+            message: 'missing required body property user'
+        });
+
+        const data = await db.fetchCoupon(request.params.code);
+        if (!data) return reply.send({
+            status: 'error',
+            message: 'coupon not found'
+        });
+
+        const user = await db.fetchAccount(request.body.user);
+        if (!user) return reply.send({
+            status: 'error',
+            message: 'user not found'
+        });
+
+        if (data.expires_at > Date.now()) return reply.send({
+            status: 'error',
+            message: 'coupon is expired'
+        });
+
+        if (data.uses === 0) return reply.send({
+            status: 'error',
+            message: 'coupon has ran out of uses'
+        });
+
+        data.uses &&= data.uses - 1;
+        if (data.coins) user.coins += data.coins;
+
+        for (let [key, val] of Object.entries(user.resources)) {
+            val = Number(val) + Number(data[key]);
+            if (val > 999_999) val = 999_999;
+            user.resources[key] = String(val);
+        }
+
+        await db.updateCoupon(data.code, data);
+        await db.updateAccount(user.email, user);
+        return reply.send({
+            status: 'success',
+            data:{ coupon: data, user }
+        });
     });
 
     api.delete('/:code', async (request, reply) => {
