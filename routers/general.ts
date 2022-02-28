54b99a022e33f5d58b61b5d58951b870edd210a4
devnote-dev <inferno0745@gmail.com>
2022-02-28 22:27:15 +0000

    feat(routers): general & auth routers

--- routers/general.ts
+++ routers/general.ts
@@ -1,7 +1,23 @@
 import { FastifyInstance } from 'fastify';
+import { Session } from '../helpers/structs';
 
-export default (api: FastifyInstance, err: (err?: Error | undefined) => void) => {
+export default (api: FastifyInstance, done: (err?: Error | undefined) => void) => {
     api.get('/', (request, reply) => {
-        const session = request.session.get
-    })
+        const session = request.session.get<Session>('account');
+
+        // TODO: add valid session check
+        reply.view('home.ejs', { ...session });
+    });
+
+    api.get('/login', (request, reply) => {
+        const session = request.session.get<Session>('account');
+        // TODO: valid check here too
+        if (session) {
+            reply.redirect('/dashboard');
+        } else {
+            reply.view('login.ejs', {});
+        }
+    });
+
+    done();
 }
